f8197e1cc392df875608f104ae30fa77586a7a2a
AlvaroPrates <alvarobirckprates@gmail.com>
2025-02-08 14:40:32 -0300

    Moving Around

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import circleshape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 import pygame
 
 class Player(circleshape.CircleShape):
@@ -17,3 +17,14 @@ class Player(circleshape.CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
